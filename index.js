76d3197e6ecfe24c630e64244bdb660558271b11
Samil Akpinar <samilakpinar6@gmail.com>
2020-09-12 19:07:43 +0300

    no message

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import Router from './src/Router';
 import {name as appName} from './app.json';
-import 'react-native-gesture-handler';
+
 AppRegistry.registerComponent(appName, () => Router);
