a6d0476ae9cc3d1b2428b136b3d8f6a0403c69af
Samil Akpinar <samilakpinar6@gmail.com>
2020-09-17 22:08:53 +0300

    Düzenleme

--- src/routers/Auth/LoginScreen.js
+++ src/routers/Auth/LoginScreen.js
@@ -167,18 +167,7 @@ const styles = StyleSheet.create({
     input: { height: '20%', },
 
 
-    container: {
-        flex: 1,
-        backgroundColor: '#003f5c',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logo: {
-        fontWeight: "bold",
-        fontSize: 50,
-        color: "#fb5b5a",
-        marginBottom: 40
-    },
+    
     inputView: {
 
         width: "80%",
@@ -192,23 +181,6 @@ const styles = StyleSheet.create({
     inputText: {
         height: 50,
         color: "white"
-    },
-    forgot: {
-        color: "white",
-        fontSize: 11
-    },
-    loginBtn: {
-        width: "80%",
-        backgroundColor: "#fb5b5a",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 40,
-        marginBottom: 10
-    },
-    loginText: {
-        color: 'white'
     }
 
 });
